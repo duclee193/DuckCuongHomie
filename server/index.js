11dfe084899a083c7d6daed9147ef9ac2cd0c11c
CuongNguyen1852003 <nttc1852003@gmail.com>
2024-11-21 11:01:05 +0700

    mongoose set up error fixed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { MongoClient } = require('mongodb');
 
+const authRoutes = require("./routes/auth.js")
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -13,11 +15,10 @@ app.use(express.static("public"));
 const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: "duckcuonghomie",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    dbName: "duckcuonghomie"   // Xóa các tùy chọn deprecated
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
+
