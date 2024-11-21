eb5bac6a35b49b967256691827799d35934493f5
CuongNguyen1852003 <nttc1852003@gmail.com>
2024-11-21 10:51:48 +0700

    configuration multer for file upload 1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,3 +4,13 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 
 const User = require("../models/User");
+
+/* Configuration Multer for File Upload */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // Use the original file name
+    },
+  });
