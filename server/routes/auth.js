6344f1ec3ccb33d77d3b2820b955e8e9cc4ec85b
CuongNguyen1852003 <nttc1852003@gmail.com>
2024-11-21 10:40:51 +0700

    Set up models folder-User.js, routes folder-auth.js and mongoose set up database

---
+++ server/routes/auth.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+
+const User = require("../models/User");
